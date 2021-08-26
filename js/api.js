9696802798fa26599aea34818d4ebb628f8a2743
developer-suhag <developersuhag@gmail.com>
2021-08-26 10:52:06 +0600

    api intro

---
+++ js/api.js
@@ -0,0 +1,9 @@
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+function loadData() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
