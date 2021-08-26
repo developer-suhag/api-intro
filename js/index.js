5fcf164ca9cb5782b1949cf688193f24aa7c69e9
developer-suhag <developersuhag@gmail.com>
2021-08-26 08:58:27 +0600

    JSON intro

--- js/index.js
+++ js/index.js
@@ -3,4 +3,29 @@ const user = {
     name: 'Amok khan',
     job: 'Khay day gomay'
 }
-console.log(user);
+
+const stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified);
+const userConvert = JSON.parse(stringified);
+// console.log(userConvert);
+
+
+const shop = {
+    name: 'Jhankar Shop',
+    address: 'America',
+    profit: 20000,
+    products: ['Python', 'JavaScript', 'React', 'Node', 'MongoDB', 'and many more'],
+    owner: {
+        ownerName: 'Jhankar Mahbub',
+        profession: 'Web Dev',
+    },
+};
+
+const shopStrigified = JSON.stringify(shop);
+
+const shopConvert = JSON.parse(shopStrigified);
+
+// console.log(shop);
+console.log(shopStrigified);
+console.log(shopConvert);
